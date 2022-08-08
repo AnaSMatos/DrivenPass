b513fceb3698e9b73660f8be0273574888ed0d39
Luana Lessa <lessalsn@gmail.com>
2022-08-08 19:04:05 -0300

    Code review

--- src/schemas/notesSchema.ts
+++
@@ -1,8 +0,0 @@
-import Joi from "joi"
-
-const notesSchema = Joi.object({
-    title: Joi.string().required().max(50),
-    note: Joi.string().required().max(1000)
-})
-
-export default notesSchema;
